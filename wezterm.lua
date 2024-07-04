0d84cd1355eb8dafa1cf922d7d4d656a49c23f81
samarth-nagar <samarthxma2009@gmail.com>
2024-07-05 01:22:33 +0530

    backup

--- wezterm.lua
+++ wezterm.lua
@@ -2,11 +2,11 @@ local wezterm = require("wezterm")
 
 local config = {
 	color_scheme = "One Dark (Gogh)",
-	window_background_opacity = 0.95,
+	window_background_opacity = .98,
 	window_decorations = "NONE",
 	hide_tab_bar_if_only_one_tab = true,
 	font_size = 16,
-	window_padding = { left = 0, right = 0, top = 3, bottom = 0 },
+	window_padding = { left = 5, right = 0, top = 4, bottom = 0 },
 	force_reverse_video_cursor = true,
 	foreground_text_hsb = {
 		hue = 1.0,
@@ -18,5 +18,19 @@ local config = {
 
 config.colors = { foreground = "#abb2bf" }
 config.font = wezterm.font("JetBrains Mono", { weight = "Regular", italic = false })
+
+config.window_background_image = '~/marek-piwnicki-wgsu3WzFZ5c-unsplash.jpg'
+
+config.window_background_image_hsb = {
+	-- Darken the background image by reducing it to 1/3rd
+	brightness = 1.3,
+
+	-- You can adjust the hue by scaling its value.
+	-- a multiplier of 1.0 leaves the value unchanged.
+	hue = 1.0,
+
+	-- You can adjust the saturation also.
+	saturation = 1.0,
+}
 
 return config
